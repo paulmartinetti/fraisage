3908d1eeea6b037b362f9a001402a72c128d2ece
Paul Martinetti <paul@digscitech.com>
2020-02-07 09:43:15 +0100

    both functions working..details to come

--- fraisGgode.js
+++ fraisGgode.js
@@ -71,15 +71,18 @@ function getGcode() {
     if (inclus) coupeA.push(oneCoupe[0]);
   }
   //Logger.log(coupeA);
+  
+  
   // 2. loop once through types, capturing Gcode sp for each type in coupeA
   var sp = "";
+  var typeA = [];
   // skipping sheetsA[0] which is Commande
   for (i=1;i<sheetsA.length;i++) {
     // e.g. Circle, sheet 2 in sheetsA, the first type sheet
     var type = sheetsA[i].getName();
     // check first element of all arrays in coupeA for Circle
     var match = false;
-    for (var j=0;j<coupeA.length;j++) {
+    for (j=0;j<coupeA.length;j++) {
        // if Circle == Circle at least once
        if (coupeA[j][0] == type) {
          match = true;
@@ -90,7 +93,7 @@ function getGcode() {
     // if match, add Gcode to sp
     if (match) {
     // Assign an sp code O01+10+i
-         sp+="O01"+(10+i)+" (sp)"+n;
+         sp+="O12"+(10+i)+" (sp)"+n;
          // process Gcode arrays to strings with line breaks
          var gStr = "";
          var gCodeA = sheetsA[i].getRange("A1:A").getValues().filter(String);
@@ -100,13 +103,43 @@ function getGcode() {
          // append to sp
          sp+=gStr;
          // spacer
-         sp+=n+n;
+         sp+=n;
+         // save type obj for pp
+         var obj = {};
+         obj.type = type;
+         obj.ind = i;
+         typeA.push(obj);
     }
   }
   
   /*** build programme principal (pp) ***/
-  // ** use coupeA for valid coupes
-  
-  //return pp+sp;
+  // debut
+  var pp = "G90 (Absolute positioning)"+n
+    +"G1 X0 Y0 F1200"+n
+    +"G1 Z0 F1200"+clrCom
+    +"(commence programme principal)"+n;
+    
+  // 1. use coupeA (array of rows in atelier) for valid coupes
+  // ignore[0] which is name
+  for (i=0;i<coupeA.length;i++){
+    // capture coupe type
+    type =coupeA[i][0];
+    // update coupe #
+    pp+=n;
+    pp+="(Coupe No. "+(i+1)+", "+type+")"+n;
+    // update vars based on index
+    for (j=1;j<coupeA[i].length;j++){
+      pp+="#1"+(10+j)+"="+coupeA[i][j]+n;
+    }
+    // for each coupe, get matching sheet index from typeA for sp call
+    for (k=0;k<typeA.length;k++){
+      if (typeA[k].type == type) {
+        pp+="M98 P12"+(10+typeA[k].ind)+" (appel sp)"+n;
+        continue;
+      }
+    }
+  }
+  pp+="M30 (fin pp)";
+  return pp+n+n+n+sp;
 }
   
